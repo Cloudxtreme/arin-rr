c479b04afe0121d89e7b3cf6987c95465fcaf134
Kevin Kirsche <Kev.Kirsche@gmail.com>
2016-04-06 20:47:53 -0400

    Remove VZ Defaults
    Signed-off-by: Kevin Kirsche <Kev.Kirsche@gmail.com>

--- lib/templates.go
+++ lib/templates.go
@@ -3,24 +3,10 @@ package irr
 const (
 	// DefaultToEmail is the e-mail address to use when emailing ARIN about the RR
 	DefaultToEmail = "rr@arin.net"
-	// DefaultToEmail = "kevin.kirsche@one.verizon.com"
-
-	// DefaultFromEmail is the email address to use when responding / sending from CRS
-	DefaultFromEmail = "crs-crew@verizon.com"
-	// DefaultNotifyEmail is who should be notified once the update goes through
-	DefaultNotifyEmail = "crs-crew@verizon.com"
-	// DefaultChangedByEmail is who should be notified once the update goes through
-	DefaultChangedByEmail = "crs-crew@verizon.com"
-	// DefaultSMTPServer is the SMTP server to use when sending the email
-	DefaultSMTPServer = "smtp.vzbi.com:smtp"
-	// DefaultMntBy is the group maintaining the entry
-	DefaultMntBy = "MNT-MCICS"
 	// DefaultSubject is the subject line for the ARIN email
 	DefaultSubject = "route"
 	// DefaultSource The source line for the ARIN email
 	DefaultSource = "ARIN"
-	// DefaultDescription the description for use in the email description
-	DefaultDescription = "Verizon Internet Services"
 
 	// ArinRouteEntryTemplate is the template to execute when sending a new route
 	// to ARIN
